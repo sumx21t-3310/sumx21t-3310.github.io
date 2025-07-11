b81429e05b7c131e1429ecad6ff6ea042c6b97ab
sumx21t <otoro180yen@gmail.com>
2025-07-11 11:10:40 +0900

    fix deploy settings

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,7 +3,7 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `Kawaii is my fuel.`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://sumx21t-3310.github.io/`
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
